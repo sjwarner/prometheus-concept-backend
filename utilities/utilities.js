d9a317c18d6f4704fd6966fdb2527a6d1c2d583a
Sam Warner <sam_warner@talktalk.net>
2022-05-09 23:21:27 +0100

    Add createNamespace endpoint

---
+++ utilities/utilities.js
@@ -0,0 +1,13 @@
+export const generateNamespace = (length = 6) => {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNPQRSTUVWXYZ123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+// module.exports = {
+//   generateNamespace: generateNamespace,
+// };
